37b61eb747acabd7c4aa1a93b7039383d5706163
Sungwoo Kuk <kuksungwoo99@naver.com>
2023-07-24 10:14:54 +0900

    Update 숫자 문자열과 영단어.py

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -8,3 +8,14 @@ def solution(s):
     
     
     return int(answer)
+
+
+##########################################################################################
+
+def solution(s):
+    words = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+
+    for i in range(len(words)):
+        s = s.replace(words[i], str(i))
+
+    return int(s)
