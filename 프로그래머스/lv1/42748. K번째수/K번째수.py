7e6003d8c1cc3a7d37a6399299ee5af94d3beb33
Sungwoo Kuk <kuksungwoo99@naver.com>
2023-08-07 09:47:50 +0900

    Update K번째수.py

--- 프로그래머스/lv1/42748. K번째수/K번째수.py
+++ 프로그래머스/lv1/42748. K번째수/K번째수.py
@@ -11,3 +11,8 @@ def solution(array, commands):
         answer.append(answer_list[k])
         
     return answer
+
+#########################################################
+
+def solution(array, commands):
+    return list(map(lambda x:sorted(array[x[0]-1:x[1]])[x[2]-1], commands))
