4e5d95fe2770f647b4c9e0f5e91242d4f2a97bb7
Sungwoo Kuk <kuksungwoo99@naver.com>
2023-08-11 10:24:25 +0900

    Update 로또의 최고 순위와 최저 순위.py

--- 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
+++ 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
@@ -36,3 +36,20 @@ def solution(lottos, win_nums):
     answer.append(rank_num(min_count))
     
     return answer
+
+##########################################################################################
+
+def solution(lottos, win_nums):
+
+    rank=[6,6,5,4,3,2,1]
+
+    cnt_0 = lottos.count(0) # lottos 안에 0의 갯수 찾기
+    ans = 0
+    for x in win_nums:
+        if x in lottos:
+            ans += 1
+    return rank[cnt_0 + ans],rank[ans] # 최고 등수 : (당첨갯수 + 0의갯수) / 최저 등수 : (당첨갯수)
+
+print(solution([44, 1, 0, 0, 31, 25], [31, 10, 45, 1, 6, 19])) # [3, 5]
+print(solution([0, 0, 0, 0, 0, 0], [38, 19, 20, 40, 15, 25])) # [1, 6]
+print(solution([45, 4, 35, 20, 3, 9], [20, 9, 3, 45, 4, 35])) # [1, 1]
