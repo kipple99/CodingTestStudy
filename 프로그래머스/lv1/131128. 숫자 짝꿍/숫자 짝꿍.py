7971ff116138ee961488a0bd7bad91f12eb154f4
Sungwoo Kuk <kuksungwoo99@naver.com>
2023-08-17 11:39:15 +0900

    Update 숫자 짝꿍.py

--- 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.py
+++ 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.py
@@ -1,3 +1,27 @@
+# 시간 초과
+def solution(X, Y):
+    X = list(X)
+    Y = list(Y)
+    str_list = []
+
+    for i in X:
+        if i in Y:
+            str_list.append(i)
+            del Y[Y.index(i)]
+    
+    if str_list:
+        answer_list = sorted(str_list, reverse=True)
+        if answer_list[0] == '0':
+            answer = '0'
+            return answer
+        answer = ''.join(answer_list)
+    else:
+        answer = '-1'
+
+    return answer
+
+########################################################################
+# 모범답안
 def solution(X, Y):
     answer = ''
 
@@ -10,3 +34,9 @@ def solution(X, Y):
         return '0'
     else :
         return answer
+
+
+print(solution('100', '2345')) # '-1'
+print(solution('100', '203045')) # '0'
+print(solution('100', '123450')) # '10'
+print(solution('12321', '42531')) # '321'
