5fa0aa7133cbc62814bf5e35900d9c39aba76421
Sungwoo Kuk <kuksungwoo99@naver.com>
2023-08-21 10:19:06 +0900

    Update 올바른 괄호.py

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
@@ -11,7 +11,13 @@ def solution(s):
                 stack.pop()
             else:
                 return False
+    # 스택에 열린 괄호 위치 데이터가 남아 있으면 해당 열린 괄호는 짝이 맞는 닫힌 괄호가 없다는 뜻이다
     while stack:
         return False
 
     return True
+
+print(solution("()()")) # True
+print(solution("(())()")) # True
+print(solution(")()(")) # False
+print(solution("(()(")) # False
