59bd11238e39bf139e85b91783ad3d482cb59a90
Sungwoo Kuk <kuksungwoo99@naver.com>
2023-08-28 12:46:00 +0900

    Update 카펫.py

--- 프로그래머스/lv2/42842. 카펫/카펫.py
+++ 프로그래머스/lv2/42842. 카펫/카펫.py
@@ -5,6 +5,15 @@ def solution(brown, yellow):
         if (total / b) % 1 == 0: # total / b = a
             a = total / b
             if a >= b:
-                if 2 * a + 2 * b == brown + 4: # 2
+                if 2 * a + 2 * b == brown + 4: # 2a + 2b = brwon + 4
                     return [int(a), b]
     return answer
+
+print(solution(10, 2)) # [4, 3]
+print(solution(8, 1)) # [3, 3]
+print(solution(24, 24))  # [8, 6]
+
+# 조건 
+# a >= b
+# yellow = (a-2)(b-2)
+# brown = ab - (a-2)(b-2) = 2a + 2b - 4
