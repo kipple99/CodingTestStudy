ab0e42463f8aa45392feaf59278fb61855896da1
Sungwoo Kuk <kuksungwoo99@naver.com>
2023-08-29 11:34:24 +0900

    Update 영어 끝말잇기.py

--- 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -14,3 +14,15 @@ def solution(n, words):
             break
     
     return [number, order]
+
+### 다른 사람의 코드
+
+def solution(n, words):
+    for p in range(1, len(words)):
+        if words[p][0] != words[p-1][-1] or words[p] in words[:p]: return [(p%n)+1, (p//n)+1]
+    else:
+        return [0,0]
+
+print(solution(3, ["tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "tank"])) # [3, 3]
+print(solution(5, ["hello", "observe", "effect", "take", "either", "recognize", "encourage", "ensure", "establish", "hang", "gather", "refer", "reference", "estimate", "executive"])) # [0, 0]
+print(solution(2, ["hello", "one", "even", "never", "now", "world", "draw"])) # [1, 3]
