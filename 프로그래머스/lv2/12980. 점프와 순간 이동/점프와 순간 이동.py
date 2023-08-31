fc39e25baf31916918059c23b92cd1ae5017081a
Sungwoo Kuk <kuksungwoo99@naver.com>
2023-08-31 17:17:35 +0900

    Update 점프와 순간 이동.py

--- 프로그래머스/lv2/12980. 점프와 순간 이동/점프와 순간 이동.py
+++ 프로그래머스/lv2/12980. 점프와 순간 이동/점프와 순간 이동.py
@@ -1,10 +1,14 @@
 def solution(n):
     ans = 0
     while(n > 0):
-        if n % 2 == 1:
+        if n % 2 == 1: # jump(배터리 소모 o) 
             n -= 1
             ans += 1
-        elif n % 2 == 0:
+        elif n % 2 == 0: # 순간이동(배터리 소모 X) 2의 배수
             n = n // 2
     
     return ans
+
+print(solution(5)) # 2
+print(solution(6)) # 2
+print(solution(5000)) # 5
