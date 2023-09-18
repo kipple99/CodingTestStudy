a892d066a90aa72ad152e979b521f1b4c737ce32
Sungwoo Kuk <kuksungwoo99@naver.com>
2023-09-18 11:45:43 +0900

    Update N개의 최소공배수.py

--- 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.py
+++ 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.py
@@ -2,6 +2,26 @@ import math
 
 def solution(arr):
     arr = sorted(arr)
+    result_gcd = arr[0]
+    new_arr = [] 
+    answer = 1
+
+    for i in arr[1:]:
+        result_gcd = math.gcd(result_gcd, i)
+    
+    for j in arr:
+        new_arr.append(j // result_gcd)
+        
+    for k in new_arr:
+        answer *= k
+        
+    answer *= result_gcd
+    
+    return answer
+
+# 모범답안
+def solution(arr):
+    arr = sorted(arr)
     sol_mul = arr[0]
     for i in arr:
         sol_mul = (i * sol_mul) // math.gcd(i, sol_mul)
